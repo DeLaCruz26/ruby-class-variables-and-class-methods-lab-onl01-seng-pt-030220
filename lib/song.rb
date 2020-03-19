ad84e3373a304610d1f010d1ef05cc1c4976cbbe
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-19 06:29:06 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    artists.uniq!
+     @@artists << artists  
+   end
+    
+  def self.genres
+    
+  end
+  
+  def self.genre_count
+    
+  end
+  
+  def self.artist_count
+    
+  end
+
+
+
+
+end
